e600b2b9d3d3348b16c61a555e8507306705dd39
ChhornDavid <davidchorn73@gmail.com>
2024-09-20 14:22:43 +0700

    Your commit message

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -30,13 +30,57 @@ class UserController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|unique:user',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+
+        $users = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return report(['data' => $users]);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $users = User::find($id);
+        if (!$users) {
+        }
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|mx:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'require|string|min:8|confirmed'
         ]);
 
         if($validator->fails()){
             throw new ValidationException($validator);
         }
 
+        $users->name = $request->input('name');
+        $users->email = $request->input('email');
+
+        if ($request->filled('password')) {
+            $users->password = bcrypt($request->input('password'));
+        }
+
+        $users->save();
+        return response(['message' => 'Save succesfully']);
+    }
+
+    public function destroy($id){
+
+        $users = User::find($id);
+        if(!$users){
+            return response()->json(['message' => 'User not found!']);
+        }
+
+        $users->delete();
+        return response()->json(['message' => 'delete successfully']);
     }
 }
